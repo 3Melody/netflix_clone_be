605a5d30bf03076b23e1610502b2ffa47e6e2ee0
dev_plug <plugeiei2003@gmail.com>
2025-08-29 12:09:03 -0700

    fix state error

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,5 @@
 // movies/movies.controller.ts
-import { Controller, Get,Param } from '@nestjs/common';
+import { Controller, Get,HttpException,HttpStatus,Param } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 @Controller('movies')
 export class MoviesController {
@@ -7,16 +7,28 @@ export class MoviesController {
   
   @Get('popular')
   async popular() {
-    return this.movies.popular();
+    try {
+      return this.movies.popular();
+    } catch (error) {
+       throw new HttpException("Failed to fetch movies", HttpStatus.BAD_GATEWAY);
+    }
   }
 
   @Get('now-playing')
   async nowPlaying() {
+    try {
     return this.movies.nowPlaying(); 
+    } catch (error) {
+      throw new HttpException("Failed to fetch movies", HttpStatus.BAD_GATEWAY);
+    }
   }
 
   @Get('details/:id')
   async details(@Param('id') id: number) {
+    try {
     return this.movies.details(id);
+    } catch (error) {
+      throw new HttpException("Failed to fetch movie details", HttpStatus.BAD_GATEWAY);
+    }
   }
 }
